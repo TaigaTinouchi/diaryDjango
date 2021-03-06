ba8188cf9a7ed2a6639ecc8b97ea90651ccbc508
TaigaTinouchi <tiger-0724h10@outlook.com>
2021-03-06 23:38:40 +0900

    add javascript file

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_DIR = BASE_DIR.("static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -119,4 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS =[
+    STATIC_DIR,
+]
 STATIC_ROOT = BASE_DIR / 'static'
